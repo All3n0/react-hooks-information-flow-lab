a4a9b7528bc5ff82f35487e0ec67d8b94c548b55
ALLAN <allankipr6@gmail.com>
2024-04-25 13:12:05 +0300

    updated header and filter js files

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header({isDarkMode,onDarkModeClick}){
+    
+  
+    return(
+        <div>
+        <header>Shopster</header>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+        </div>
+    )
+}
